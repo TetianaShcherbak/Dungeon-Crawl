972aece56e2102b0363ff7351f812d9f9413471f
mrshyian <shyvlad1994@gmail.com>
2022-03-23 08:23:41 +0100

    camera move bug fixed

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -97,7 +97,7 @@ public class Main extends Application {
                 }else if (playerPositionX<11 && playerPositionY>23){
                     windowX = x;
                     windowY = y+14;;
-                }else if (playerPositionX>34 && playerPositionY>23){ //do naprawy
+                }else if (playerPositionX>34 && playerPositionY>20){
                     windowX = x+23;
                     windowY = y+14;;
                 } else if (playerPositionX<11){
