8086cc64a409afda1d950c65e116bf3b6d9086b9
justyna-eevee <justyna.zaczyk@onet.pl>
2022-03-23 13:46:50 +0100

    random ghost move

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -3,7 +3,10 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 
+import java.util.Random;
+
 public class Ghost extends Actor{
+    private Random random = new Random();
     public Ghost(Cell cell) {
         super(cell);
     }
@@ -11,10 +14,21 @@ public class Ghost extends Actor{
     @Override
     public void move(int dx, int dy) {
         Cell nextCell = this.getCell().getNeighbor(dx, dy);
-        if ((nextCell.getType() == CellType.FLOOR)) {
             this.getCell().setCellContent(null);
             nextCell.setCellContent(this);
             this.setCell(nextCell);
+    }
+
+    public void randomMove(){
+        while (true){
+            int dx = random.nextInt( 2);
+            int dy = Math.abs(1 - dx);
+            Cell nextCell = this.getCell().getNeighbor(dx, dy);
+            if ((nextCell.getType() == CellType.FLOOR) || (nextCell.getType() == CellType.WALL)
+                    || (nextCell.getType() == CellType.PINES) || (nextCell.getType() == CellType.RIVERBODY)){
+                move(dx, dy);
+                break;
+            }
         }
     }
 
