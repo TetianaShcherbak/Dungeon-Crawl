f5698d1f896678fff2fdf36aa073f4b55c53fce9
justyna-eevee <justyna.zaczyk@onet.pl>
2022-03-23 15:26:19 +0100

    ghost random move

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -3,13 +3,17 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Ghost extends Actor{
     private Random random = new Random();
     public Ghost(Cell cell) {
         super(cell);
+        createMoves();
     }
+    private List<int[]> moves = new ArrayList<>();
 
     @Override
     public void move(int dx, int dy) {
@@ -19,12 +23,25 @@ public class Ghost extends Actor{
             this.setCell(nextCell);
     }
 
+    private void createMoves(){
+        moves.add(new int[]{0, 1});
+        moves.add(new int[]{0, -1});
+        moves.add(new int[]{-1, 0});
+        moves.add(new int[]{1, 0});
+    }
+
     public void randomMove(){
         while (true){
-            int dx = random.nextInt( 2);
-            int dy = Math.abs(1 - dx);
+            int move = random.nextInt(4);
+            int dx = moves.get(move)[0];
+            int dy = moves.get(move)[1];
             Cell nextCell = this.getCell().getNeighbor(dx, dy);
-            if ((nextCell.getType() == CellType.FLOOR) || (nextCell.getType() == CellType.WALL)
+            if (nextCell.getX() > nextCell.getGameMap().getWidth()
+                    || nextCell.getY() > nextCell.getGameMap().getHeight()){
+                continue;
+            }
+            if (((nextCell.getType() == CellType.FLOOR) && nextCell.getCellContent() == null)
+                    || (nextCell.getType() == CellType.WALL) || (nextCell.getType() == CellType.OAKS)
                     || (nextCell.getType() == CellType.PINES) || (nextCell.getType() == CellType.RIVERBODY)){
                 move(dx, dy);
                 break;
